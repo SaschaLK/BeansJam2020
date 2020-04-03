4bd0807e91802435db73934a15dd66ead407dc18
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-04-03 23:07:36 +0200

    Level Generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -2,17 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class MapGenerator : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    [Range(0,1)] public float thresholdObstacle;
+    [Range(0, 1)] public float thresholdSpawner;
+
+    public List<GameObject> wallTilesNESW = new List<GameObject>();
+    public List<GameObject> floorTiles = new List<GameObject>();
+    public List<GameObject> obstacles = new List<GameObject>();
+    public GameObject spawner;
+
+    //private bool[,] obstacleMap;
+    private Dictionary<Vector2, bool> obstacleMap = new Dictionary<Vector2, bool>();
+
+    private void Start() {
+        //obstacleMap = new bool[width+1, height+1];
+
+        GenerateWalls();
+        GenerateFloor();
+        GenerateEmptyMap();
+        GenerateObstacles();
+        GenerateSpawners();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void GenerateWalls() {
+        for(int x = -1; x <= width +1; x++) {
+            for(int y = -1; y <= height +1; y++) {
+                //Northern walls
+                if (x != -1 && x != width + 1 && y == height + 1) {
+                    Instantiate(wallTilesNESW[0], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //NorthEastern Wall
+                else if (x == width + 1 && y == height + 1) {
+                    Instantiate(wallTilesNESW[1], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //Eastern Wall
+                else if (x == width + 1 && y != height + 1 && y != -1) {
+                    Instantiate(wallTilesNESW[2], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //SouthEastern Wall
+                else if (x == width + 1 && y == -1) {
+                    Instantiate(wallTilesNESW[3], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //Southern Wall
+                else if (x != -1 && x != width + 1 && y == -1) {
+                    Instantiate(wallTilesNESW[4], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //SouthWestern Wall
+                else if (x == -1 && y == -1) {
+                    Instantiate(wallTilesNESW[5], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //Western Wall
+                else if (x == -1 && y != -1 && y != height + 1) {
+                    Instantiate(wallTilesNESW[6], new Vector3(x, y), Quaternion.identity, transform);
+                }
+                //NorthWestern Wall
+                else if (x == -1 && y == height + 1) {
+                    Instantiate(wallTilesNESW[7], new Vector3(x, y), Quaternion.identity, transform);
+                }
+            }
+        }
+    }
+
+    private void GenerateFloor() {
+        for(int x = 0; x <= width; x++) {
+            for(int y = 0; y <= height; y++) {
+                int selection = Random.Range(0, floorTiles.Count);
+                Instantiate(floorTiles[selection], new Vector3(x, y), Quaternion.identity, transform);
+            }
+        }
+    }
+
+    private void GenerateEmptyMap() {
+        for (int x = 0; x <= width; x++) {
+            for (int y = 0; y <= height; y++) {
+                obstacleMap.Add(new Vector2(x, y), false);
+            }
+        }
+    }
+
+    private void GenerateObstacles() {
+        float seed = Random.Range(0f, 1f);
+        for(int x = 0; x <= width; x++) {
+            for(int y = 0; y <= height; y++) {
+                if(Mathf.PerlinNoise(x * 0.1f + seed, y * 0.1f + seed) > thresholdObstacle) {
+                    obstacleMap[new Vector2(x,y)] = true;
+                    Instantiate(obstacles[0], new Vector3(x,y), Quaternion.identity, transform);
+                }
+            }
+        }
+    }
+
+    private void GenerateSpawners() {
+        foreach(KeyValuePair<Vector2, bool> tile in obstacleMap) {
+            if (!tile.Value) {
+                if(Random.Range(0f,1f) > thresholdSpawner) {
+                    Instantiate(spawner, new Vector3(tile.Key.x, tile.Key.y), Quaternion.identity, transform);
+                }
+            }
+        }
     }
 }
