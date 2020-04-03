a0a6ca428f6cd4f415f222c0d90d598a2aa30eea
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-04-04 00:37:53 +0200

    Player prefab changes with lights - simple mob spawner with time

---
+++ Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour {
+
+    public List<GameObject> mobs;
+
+    public float spawnDelay;
+
+    private void Start() {
+        StartCoroutine(SpawnMob());
+    }
+
+    private IEnumerator SpawnMob() {
+        yield return new WaitForSecondsRealtime(spawnDelay);
+    }
+}
