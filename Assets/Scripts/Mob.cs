9e0db821f002258f7aafb8c1010c70cff5b983b9
Thomas Eichler <necro1895@gmail.com>
2020-04-04 14:45:39 +0200

    Animation Mob

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 using Pathfinding;
 
 public class Mob : MonoBehaviour {
+
+    AIPath _AIPath;
+    Animator _Animator;
+
     private void Start() {
         if (MobManager.instance != null && MobManager.instance.player != null) {
             GetComponent<AIDestinationSetter>().target = MobManager.instance.player;
             MobManager.instance.mobs.Add(gameObject);
+            _AIPath = GetComponentInChildren<AIPath>();
         }
+        _Animator = GetComponentInChildren<Animator>();
     }
 
     public void Killed() {
         MobManager.instance.kills++;
+    }
+
+    public void Update()
+    {
+        if (_AIPath == null)
+        {
+            _AIPath = GetComponentInChildren<AIPath>();
+        }
+
+        var isWalking = _AIPath.canMove;
+
+        if (_Animator != null)
+        {
+            _Animator.SetBool("IsWalking", isWalking);
+        }
     }
 
 }
