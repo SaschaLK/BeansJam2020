dfa00ca2247ce5b0cc571bf4b538f195b686125f
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-04-04 14:30:09 +0200

    Change real work in progreess

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 using Pathfinding;
 
 public class Mob : MonoBehaviour {
-
-    //public float speed;
-
-    //private Transform player;
-
     private void Start() {
-        if (MobManager.instance != null && MobManager.instance.player != null)
-        {
+        if (MobManager.instance != null && MobManager.instance.player != null) {
             GetComponent<AIDestinationSetter>().target = MobManager.instance.player;
+            MobManager.instance.mobs.Add(gameObject);
         }
-        //player = MobManager.instance.player;
     }
 
-    private void Update() {
-        //transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    public void Killed() {
+        MobManager.instance.kills++;
     }
 
 }
--- Assets/Scripts/MobManager.cs
+++ Assets/Scripts/MobManager.cs
@@ -1,17 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class MobManager : MonoBehaviour {
 
     public static MobManager instance;
 
     public Transform player;
+    public bool isOverworld = true;
     public bool spawning;
     public int spawnableMobs;
+    public List<GameObject> mobs;
+    public int kills;
 
     private void Awake() {
         instance = this;
     }
+
+    private void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            ChangeRealm();
+        }
+    }
+
+    public void ChangeRealm() {
+        isOverworld = !isOverworld;
+        foreach(GameObject mob in mobs) {
+            if(!isOverworld && mob.tag == "Overworld") {
+                mob.GetComponent<AIPath>().canMove = false;
+            }
+            else if (isOverworld && mob.tag == "Overworld") {
+                mob.GetComponent<AIPath>().canMove = true;
+            }
+        }
+    }
+
+
 
 }
