8f8058caf2a2e1039786f4d4d4c465e659ccb356
Philipp Seincher <philippseincher@googlemail.com>
2020-04-04 12:58:35 +0200

    Menu
    Added Menu implementation
    Movement Config can be accessed with PlayerPrefs.GetInt("bjMovPref");
    When returning 0 it meant to be in globally mode and returning 1 it means to go into Local or relative mode

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void LoadNextLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetMovementPreference(int pref)
+    {
+        PlayerPrefs.SetInt("bjMovPref", pref);
+    }
+}
