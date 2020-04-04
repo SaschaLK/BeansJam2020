d3ac623fce8de42fe43c785dbf94174e8a1a2c95
Thomas Eichler <necro1895@gmail.com>
2020-04-04 10:27:25 +0200

    Umstellung Movement auf Rigidbody2D

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -34,11 +34,15 @@ public class PlayerBehaviour : MonoBehaviour
 
     float _RemainCoolDown = 0f;
     Vector2 _Direction;
+    Vector2 _Movement;
+
+    Rigidbody2D _Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         Projectiles = new List<ItemSlot>();
+        _Rigidbody = this.GetComponent<Rigidbody2D>();
 
         if (ProjectileStone != null)
         {
@@ -73,20 +77,22 @@ public class PlayerBehaviour : MonoBehaviour
         var angle = this.AngleBetweenTwoPoints(_PositionLookAt, transform.position);
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
 
+        _Movement = new Vector2(horizontal, vertical);
+
         //Calculates target movement vector
-        switch(MovementType)
+        switch (MovementType)
         {
             case MovementType.Global:
                 {
-                    var translation = (Vector2.up * vertical + Vector2.right * horizontal) * MovementSpeed * dt;
-                    this.gameObject.transform.Translate(translation, Space.World);
+                    //var translation = (Vector2.up * vertical + Vector2.right * horizontal) * MovementSpeed * dt;
+                    //this.gameObject.transform.Translate(translation, Space.World);
                 }
                 break;
 
             case MovementType.LookAndMove:
                 {
-                    var translation = (transform.up * vertical * MovementSpeed + transform.right * horizontal * StrafeSpeed) * dt;
-                    this.gameObject.transform.Translate(translation, Space.World);
+                    //var translation = (transform.up * vertical * MovementSpeed + transform.right * horizontal * StrafeSpeed) * dt;
+                    //this.gameObject.transform.Translate(translation, Space.World);
                 }
                 break;
         }
@@ -100,6 +106,23 @@ public class PlayerBehaviour : MonoBehaviour
             }
         }
         
+    }
+
+    private void FixedUpdate()
+    {
+        var dt = Time.fixedDeltaTime;
+        var movement = _Movement;
+
+        switch (MovementType)
+        {
+            case MovementType.LookAndMove:
+                movement = (transform.up * _Movement.y + transform.right * _Movement.x).normalized;
+                break;
+        }
+        var finalMovement = (Vector2)transform.position + movement * MovementSpeed * dt;
+
+        _Rigidbody.MovePosition(finalMovement);
+
     }
 
     public void Fire()
